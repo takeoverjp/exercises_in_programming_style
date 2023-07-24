4af930758371bfbbd3c1c4aa5f1082d4990853a3
Takeo Kondo <takeoverjp@gmail.com>
2023-07-24 22:09:53 +0900

    Add 07.code_golf.py

---
+++ 07.code_golf.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import re, sys, collections
+stops = open('stop_words.txt').read().split(',')
+words = re.findall(r'[a-z]{2,}', open(sys.argv[1]).read().lower())
+counts = collections.Counter(w for w in words if w not in stops)
+for (w, c) in counts.most_common(25):
+    print(w, '-', c)
